19af0dd261fc8741ed952015f448317c6a72f83c
Tataz2 <126407007+Tataz2@users.noreply.github.com>
2025-02-05 14:40:06 +0200

    Add files via upload

---
+++ Winget-Install-App-System-Context.ps1
@@ -0,0 +1,49 @@
+﻿
+# Install or upgrade software by using WinGet. Can be run in System context because
+# the script tries to found the winget.exe from C:\Program Files\WindowsApps\ .
+
+
+
+[cmdletbinding()]
+param(
+    [parameter(
+        Mandatory         = $false,
+        ValueFromPipeline = $true)]
+    $pipelineInput,
+    [string]$ID = "noID",
+    [switch]$help
+)
+
+Process {
+
+    # Print help 
+    If ( "$ID" -eq "noID" -and $pipelineInput -eq $null) {
+        Write-Host "Please give the Id of the package you want to install or upgrade by using Winget."
+        Write-Host "        -id <Id>"
+        Write-host " eg.    -id KDE.Okular"
+        Write-host "App Id is also accepted from pipeline."
+        exit 0
+     }
+
+    # Find path for winget.exe so winget can be run in system conntext.
+    $WingetPath = Resolve-Path "C:\Program Files\WindowsApps\Microsoft.DesktopAppInstaller_*_x64__8wekyb3d8bbwe\winget.exe"
+    If ($ResolveWingetPath){
+            Write-Host "Found Winget $WingetPath"
+       } Else {
+           Write-Host "Did not found winget.exe"
+           exit 1
+    }
+
+
+    If ( "$ID" -eq "noID" -and $pipelineInput -ne $null) {
+        Write-Host "No parameter -id given but found input from pipeline."
+        Write-Host "Pipeline input $pipelineInput"
+        $ID = $pipelineInput
+     }
+
+    Write-Host "Trying to install or uprade $Id"
+    & $WingetPath install $ID --accept-source-agreements --source winget
+
+
+} # Process ends
+
